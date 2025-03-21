7f3be2d32dfa2321d7a9349349a70c104e7ff2d9
Abhinav singh <asingh308@nait.ca>
2025-03-21 02:20:44 -0600

    For/OF loop examples

---
+++ JS-defintive-guide/CH-5/loops.js
@@ -0,0 +1,23 @@
+//for/of with strings
+
+let frequency = {};
+for (let letter of 'mississippi') {
+    if (frequency[letter]) {
+        frequency[letter]++;
+    }
+    else {
+        frequency[letter] = 1;
+    }
+}
+frequency;
+
+
+//for/of with set and map
+
+let text = "NA na na na na na na na Batman!";
+let wordSet = new Set(text.split(" "));
+let unique = [];
+for (let word of wordSet) {
+    unique.push(word);
+}
+unique
